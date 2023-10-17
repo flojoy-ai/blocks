daa6cb969e0b125ab327fd6a00c9ad599f06879c
Joey Yu <joey@jyu.dev>
2023-10-17 17:32:37 -0400

    add shiki config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -62,6 +62,19 @@ export default defineConfig({
     }),
     react(),
   ],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: ["python"],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
   vite: {
     resolve: {
       alias: {
