3458e365b76bc6f757afbfe2c67017d4b429da10
JeffDotPng <zhangjeff2014@gmail.com>
2023-10-18 16:00:59 -0400

    add a download button for example apps

--- docs/src/components/app-display.tsx
+++ docs/src/components/app-display.tsx
@@ -1,6 +1,7 @@
 import ReactFlow, { Background, Controls, MiniMap } from "reactflow";
 import "reactflow/dist/style.css";
 import nodeTypes from "./nodes/nodeTypes";
+import { Download } from "lucide-react";
 
 type AppDisplayProps = {
   app: any;
@@ -27,21 +28,42 @@ const AppDisplay = ({ app }: AppDisplayProps) => {
   const nodes = appObject["nodes"];
   const edges = appObject["edges"];
 
+  const download = () => {
+    const jsonString = JSON.stringify(app, null, 2);
+    const blob = new Blob([jsonString], { type: "application/json" });
+
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "app.json";
+    a.click();
+
+    URL.revokeObjectURL(url);
+  };
+
   return (
-    <div className="flex justify-center">
-      <div style={{ width: "100vw", height: 400 }} className="not-content">
-        <ReactFlow
-          nodes={nodes}
-          nodeTypes={nodeTypes}
-          edges={edges}
-          minZoom={0.25}
-          fitView
-          proOptions={{ hideAttribution: true }}
-        >
-          <FlowMiniMap />
-          <Background />
-          <Controls />
-        </ReactFlow>
+    <div className="mt-6">
+      <div className="flex items-center">
+        <h4>Example app</h4>
+        <Download onClick={download} className="ml-2 cursor-pointer !mt-1" />
+      </div>
+      <div>
+        <div className="flex justify-center">
+          <div style={{ width: "100vw", height: 400 }} className="not-content">
+            <ReactFlow
+              nodes={nodes}
+              nodeTypes={nodeTypes}
+              edges={edges}
+              minZoom={0.25}
+              fitView
+              proOptions={{ hideAttribution: true }}
+            >
+              <FlowMiniMap />
+              <Background className="bg-gray-50" />
+              <Controls />
+            </ReactFlow>
+          </div>
+        </div>
       </div>
     </div>
   );
