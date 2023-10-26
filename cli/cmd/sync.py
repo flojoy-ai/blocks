ca052897754b374a267ba240de62d804b5a62510
JeffDotPng <zhangjeff2014@gmail.com>
2023-10-26 16:50:08 -0400

    inline functions

--- cli/cmd/sync.py
+++ cli/cmd/sync.py
@@ -27,7 +27,9 @@ def sync():
     print("Generating docstring.json for all the blocks...")
     success = generate_docstring_json()
     if not success:
-        print(f"{ERR_STRING} Please fix all the docstring errors before syncing.")
+        print(
+            f"{ERR_STRING} Please fix all the docstring errors before syncing."
+        )
         sys.exit(1)
 
     print(f"Cleaning the blocks section except all the {KEEP_FILES} files.")
@@ -56,62 +58,39 @@ def sync():
             block_folder_path = root.split("blocks", 1)[1].strip("/")
             block_category = block_folder_path.split("/")[0]
             autogen = block_category in AUTOGEN_CATEGORIES
-            required_files = (
-                REQUIRED_EXAMPLE_FILES
-                if autogen
-                else REQUIRED_EXAMPLE_FILES + REQUIRED_SKIP_IF_AUTOGEN
-            )
+            required_files = (REQUIRED_EXAMPLE_FILES
+                              if autogen else REQUIRED_EXAMPLE_FILES +
+                              REQUIRED_SKIP_IF_AUTOGEN)
 
             for required_file in required_files:
                 if not os.path.exists(os.path.join(root, required_file)):
-                    print(f"{ERR_STRING} No {required_file} found for {file_name}")
+                    print(
+                        f"{ERR_STRING} No {required_file} found for {file_name}"
+                    )
                     sys.exit(1)
 
-            desc = _get_short_description(root)
-            _write_doc_page(block_folder_path, file_name, desc, autogen)
+            with open(os.path.join(root, "docstring.json"), "r") as f:
+                description = json.load(f)["short_description"]
+
+            target_md_file = DOCS_FOLDER + file_name + ".mdx"
+            os.makedirs(os.path.dirname(target_md_file), exist_ok=True)
+
+            with open(target_md_file, "w") as f:
+                # Write the content of the markdown file
+                result = (BlockDocsBuilder(
+                    block_name=file_name,
+                    block_folder_path=block_folder_path,
+                    description=description,
+                ).add_python_docs_display().add_python_code())
+
+                if not autogen:
+                    result = result.add_example_app()
+
+                f.write(result.build())
 
             total_synced_pages += 1
 
     print("Almost done! Doing some housekeeping...")
-    _prune_unneeded_files()
-
-    print(f"[bold green] Successfully synced {total_synced_pages} block pages!")
-
-
-def _get_short_description(path: str):
-    """Get the short description for a block"""
-    with open(os.path.join(path, "docstring.json"), "r") as f:
-        description = json.load(f)["short_description"]
-    return description
-
-
-def _write_doc_page(path: str, file_name: str, description: str, autogen: bool):
-    """Create the markdown template file in docs"""
-
-    target_md_file = DOCS_FOLDER + path + ".mdx"
-
-    os.makedirs(os.path.dirname(target_md_file), exist_ok=True)
-
-    with open(target_md_file, "w") as f:
-        # Write the content of the markdown file
-        result = (
-            BlockDocsBuilder(
-                block_name=file_name,
-                block_folder_path=path,
-                description=description,
-            )
-            .add_python_docs_display()
-            .add_python_code()
-        )
-
-        if not autogen:
-            result = result.add_example_app()
-
-        f.write(result.build())
-
-
-def _prune_unneeded_files():
-    """Deletes all .DS_Store files and empty directories."""
     for root, _, files in os.walk("."):
         for file in files:
             if file == ".DS_Store":
@@ -125,3 +104,6 @@ def _prune_unneeded_files():
     for dirpath, dirnames, filenames in os.walk(BLOCKS_FOLDER):
         if not filenames and not dirnames:
             os.rmdir(dirpath)
+
+    print(
+        f"[bold green] Successfully synced {total_synced_pages} block pages!")
