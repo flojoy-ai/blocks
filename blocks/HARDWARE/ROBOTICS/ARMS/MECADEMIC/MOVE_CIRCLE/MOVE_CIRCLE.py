015e9d7b010a293807f6db610fc5a20f52813136
Sasha Aleshchenko <45843308+TheBigSasha@users.noreply.github.com>
2023-10-27 03:06:01 -0700

    mecademic blocks ported from nodes repo

---
+++ blocks/HARDWARE/ROBOTICS/ARMS/MECADEMIC/MOVE_CIRCLE/MOVE_CIRCLE.py
@@ -0,0 +1,46 @@
+from typing import Optional
+from flojoy import flojoy, TextBlob
+from PYTHON.utils.mecademic_state.mecademic_calculations import get_circle_positions
+from PYTHON.utils.mecademic_state.mecademic_state import query_for_handle
+from PYTHON.utils.mecademic_state.mecademic_helpers import safe_robot_operation
+
+
+@safe_robot_operation
+@flojoy(deps={"mecademicpy": "1.4.0"})
+def MOVE_CIRCLE(
+        ip_address: TextBlob,
+        radius: Optional[float] = 0.0,
+        revolutions: Optional[float] = 1.0,
+) -> TextBlob:
+    """
+    The Move circle node moves in a circle relative to a reference plane.
+
+    Inputs
+    ------
+    ip_address: TextBlob
+        The IP address of the robot arm.
+
+    Parameters:
+    -------
+    radius: Optional[float]
+        The radius of the circle. If not specified, the default value of 0.0 is used.
+    revolutions: Optional[float]
+        The number of revolutions to make. If not specified, the default value of 1.0 is used.
+
+    Returns
+    -------
+    ip_address
+        The IP address of the robot arm.
+
+    """
+    robot = query_for_handle(ip_address)
+
+    X, Y, Z, alpha, beta, gamma = robot.GetPose()
+
+    positions = get_circle_positions(radius, revolutions, (X, Y, Z))
+    for position in positions:
+        robot.MoveLin(x=position[0], y=position[1], z=position[2],
+                      alpha=alpha, beta=beta, gamma=gamma)
+    robot.MoveLin(x=X, y=Y, z=Z, alpha=alpha, beta=beta, gamma=gamma)
+
+    return ip_address
---
+++ blocks/HARDWARE/ROBOTICS/ARMS/MECADEMIC/SET_TRF/SET_TRF.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from flojoy import flojoy, TextBlob
+from PYTHON.utils.mecademic_state.mecademic_state import query_for_handle
+from PYTHON.utils.mecademic_state.mecademic_helpers import safe_robot_operation
+
+
+@safe_robot_operation
+@flojoy(deps={"mecademicpy": "1.4.0"})
+def SET_TRF(ip_address: TextBlob, rf_x: Optional[float] = 0.0, rf_y: Optional[float] = 0.0, rf_z: Optional[float] = 0.0, rf_a: Optional[float] = 0.0, rf_b: Optional[float] = 0.0, rf_g: Optional[float] = 0.0) -> TextBlob:
+    """
+     The SET_TRF node sets the robot arm's reference frame.
+
+     Inputs
+     ------
+     ip_address
+         The IP address of the robot arm.
+
+     Parameters
+     ------
+    rfX: Optional[float]
+        The X coordinate of the reference plane. If not specified, the default value of 0.0 is used.
+    rfY: Optional[float]
+        The Y coordinate of the reference plane. If not specified, the default value of 0.0 is used.
+    rfZ: Optional[float]
+        The Z coordinate of the reference plane. If not specified, the default value of 0.0 is used.
+    rfAlpha: Optional[float]
+        The alpha angle of the reference plane. If not specified, the default value of 0.0 is used.
+    rfBeta: Optional[float]
+        The beta angle of the reference plane. If not specified, the default value of 0.0 is used.
+    rfGamma: Optional[float]
+        The gamma angle of the reference plane. If not specified, the default value of 0.0 is used.
+
+    Returns
+     -------
+     ip_address
+         The IP address of the robot arm.
+
+    """
+    robot = query_for_handle(ip_address)
+    robot.SetTRF(rf_x, rf_y, rf_z, rf_a, rf_b, rf_g)
+    return ip_address
