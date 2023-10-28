f035342db7e03aae5655f8c4ab27951de894fa83
Jinwon Lee <jinwonlee0916@gmail.com>
2023-10-28 11:45:03 -0400

    Vector and Vector_2_Scalar nodes done

--- blocks/DATA/GENERATION/SIMULATIONS/VECTOR/VECTOR.py
+++ blocks/DATA/GENERATION/SIMULATIONS/VECTOR/VECTOR.py
@@ -1,8 +1,9 @@
 from flojoy import flojoy, Vector
 from numpy import array
+from typing import Literal
 
 @flojoy
-def VECTOR(elements : str = "") -> Vector:
+def VECTOR(elements : str = "", elements_type : Literal["boolean", "integer"] = "boolean") -> Vector:
     """The VECTOR node creats a vector type data given the elements
 
     Parameters
@@ -17,15 +18,21 @@ def VECTOR(elements : str = "") -> Vector:
     """
 
     elements_list = elements.split(",")
+
+    if elements_type == "boolean":
+        all_bool = [element.lower() == "true" for element in elements_list]
+
+        if  all(isinstance(element, bool) for element in all_bool):
+            return Vector(v=array(all_bool))
+        
+        raise ValueError(f"all elements of the vector must be in boolean or integer type: {all_bool}")
+        
+    elif elements_type == "integer":
+        all_int = [int(element) for element in elements_list if element.isnumeric()]
+
+        if all(isinstance(element, int) for element in all_int):
+            return Vector(v=array(all_int))
+        
+        raise ValueError(f"all elements of the vector must be in boolean or integer type: {all_int}")
     
-    all_int = [int(element) for element in elements_list if element.isnumeric()]
-
-    if all(isinstance(element, int) for element in all_int):
-        return Vector(v=array(all_int))
-    
-    all_bool = [element.lower() == "true" for element in elements_list]
-
-    if  all(isinstance(element, bool) for element in all_bool):
-        return Vector(v=array(all_bool))
-
-    raise ValueError(f"all elements of the vector must be in boolean or integer type: {all_int}")
+    raise ValueError("all elements of the vector must be in boolean or integer type")
--- blocks/ETL/TRANSFORM/TYPE_CASTING/VECTOR_2_SCALAR/VECTOR_2_SCALAR.py
+++ blocks/ETL/TRANSFORM/TYPE_CASTING/VECTOR_2_SCALAR/VECTOR_2_SCALAR.py
@@ -1,3 +1,4 @@
+from numpy import bool_, int_
 from flojoy import flojoy, Vector, Scalar
 
 
@@ -15,16 +16,21 @@ def VECTOR_2_SCALAR(default: Vector) -> Scalar:
     Scalar
         The scalar that is generated from the given vector.
     """
-    all_boolean = all(isinstance(element, bool) for element in default.v)
+    all_boolean = all(isinstance(element, bool_) for element in default.v)
 
     if all_boolean:
-        decimal_num = sum(bit << i for i, bit in enumerate(default.v))
-        return Scalar(c=decimal_num)
+        binary_string = ''.join(['1' if bit else '0' for bit in default.v])
+        print(default.v)
+        decimal_number = int(binary_string, 2)      
+        return Scalar(c=decimal_number)
 
-    all_boolean = all(isinstance(element, int) for element in default.v)
+    all_int = all(isinstance(element, int_) for element in default.v)
 
-    if all_boolean:
+    if all_int:
         decimal_num = sum(element for element in default.v)
         return Scalar(c=decimal_num)
+    
+    print("---------------------------------------",all_boolean)
+    print(default.v)
 
-    raise ValueError("all elements of the vector must be in boolean or integer type")
+    raise ValueError(f"all elements of the vector must be in boolean or integer type")
