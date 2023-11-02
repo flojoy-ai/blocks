c9cbe3c71bacaa86f2677fafe5aa2e96ee3aa892
Dallas Strandell <41165864+argenate@users.noreply.github.com>
2023-11-02 08:36:59 -0700

    record

---
+++ blocks/HARDWARE/AUDIO/RECORD_AUDIO/RECORD_AUDIO.py
@@ -0,0 +1,65 @@
+from flojoy import flojoy, DataContainer, TextBlob
+from typing import Optional
+import pyaudio
+import wave
+
+
+@flojoy(deps={"pyaudio": "0.2.13"})
+def RECORD_AUDIO(
+    file_name: str = "output",
+    default: Optional[DataContainer] = None,
+) -> Optional[DataContainer]:
+    """
+
+    Parameters
+    ----------
+    file_name : str
+        File name of the output audio file.
+
+    Returns
+    -------
+    TextBlob
+        Filename and path of the recording.
+    """
+
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print("Recording")
+
+    stream = p.open(
+        format=sample_format,
+        channels=channels,
+        rate=fs,
+        frames_per_buffer=chunk,
+        input=True,
+    )
+
+    frames = []  # Initialize array to store frames
+    # Store data in chunks for 3 seconds
+    for _ in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print("Finished recording")
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(f"{file_name}.wav", "wb")
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+
+    return TextBlob(text_blob=file_name)
