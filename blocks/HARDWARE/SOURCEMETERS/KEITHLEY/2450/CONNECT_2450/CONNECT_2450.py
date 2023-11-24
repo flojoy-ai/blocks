1c97c0f401a8764e1c09c9c1d0b609f7f2672b3e
Dallas Strandell <41165864+argenate@users.noreply.github.com>
2023-11-24 11:43:22 -0800

    reset and connect

--- blocks/HARDWARE/SOURCEMETERS/KEITHLEY/2450/CONNECT_2450/CONNECT_2450.py
+++ blocks/HARDWARE/SOURCEMETERS/KEITHLEY/2450/CONNECT_2450/CONNECT_2450.py
@@ -22,6 +22,13 @@ def CONNECT_2450(
     device_addr = device.get_address()
 
     smu = DeviceConnectionManager.tm.add_smu(device_addr)
+    smu.write("*LANG TSP")
+    lang = smu.query("*LANG?")
+    if lang != "TSP":
+        raise ValueError(
+            "Command set language not set correctly. Try setting to 'TSP' manually."
+        )
+
     DeviceConnectionManager.register_connection(device, smu)
 
     return TextBlob(text_blob=device_addr)
